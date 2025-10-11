d371c3886f00817c9cea809217fdd4c00fd860de
Maxim Rodionov <rodionov.maxim05@gmail.com>
2025-10-11 19:30:19 +0300

    chore: add formatter settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,18 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+int main() {
+	auto window =
+		sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+	window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
+	while (window.isOpen()) {
+		while (const std::optional event = window.pollEvent()) {
+			if (event->is<sf::Event::Closed>()) {
+				window.close();
+			}
+		}
 
-        window.clear();
-        window.display();
-    }
+		window.clear();
+		window.display();
+	}
 }
